be868cf40a351017159e1c49f6cec6fdcbbe198e
kgu <kevinguicherd@gmail.com>
2014-01-07 16:01:15 +0100

    Mise en place de l'arborescence

---
+++ src/public/index.php
@@ -0,0 +1,7 @@
+<?php 
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(dirname(__DIR__)));
+define('SRC_PATH', ROOT_PATH . DS . 'src');
+
+require_once ROOT_PATH . DS . 'vendor' . DS . 'autoload.php';
